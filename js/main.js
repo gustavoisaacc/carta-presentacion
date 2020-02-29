f97886644048987ee310749bb3aedd33335acb11
gustavoisaac <gustavochaile7@gmail.com>
2020-02-28 21:44:39 -0300

    se termino el slide

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
     var banner = {
         padre: $('#banner'),
         numeroSlider: $('#banner').children('.slider').length,
-        posision: 1,
+        posicion: 1,
     }
 
     banner.padre.children('.slider').first().css({
@@ -23,4 +23,50 @@
     $(window).resize(function(){
         altoBanner();
     })
+
+
+    //---------------
+    //--BANNER--
+    //--------------
+
+    /*BOTON SIGUIENTE*/
+
+    $('#banner-next').on('click', function(e){
+
+       if(banner.posicion < banner.numeroSlider){
+
+        banner.padre.children().not('.active').css({
+            'left': '100%',
+        });
+
+        $('#banner .active').removeClass('active').next().addClass('active').animate({
+            'left': '0%',
+        });
+
+        $('#banner .active').prev().animate({
+            'left': '-100%',
+        })
+
+        banner.posicion = banner.posicion + 1
+
+       }else{
+           $('#banner .active').animate({
+               'left': '-100%'
+           })
+
+           banner.padre.children().not('.active').css({
+            'left': '100%',
+            });
+           
+
+           $('#banner .active').removeClass('active');
+           banner.padre.children('.slider').first().addClass('active').animate({
+               'left': '0%',
+           })
+
+           banner.posicion = 1
+       }
+         
+    })
+
  })
